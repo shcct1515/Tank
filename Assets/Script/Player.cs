c310a3607598ba6617a942175ba552eb218a8107
shcct <shcct1254@gmail.com>
2024-11-11 20:46:12 +0700

    add bullet and script to shoot bullet

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -5,10 +5,36 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float offsetAngle = -90f;
+
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 10f;
+    public float fireRate = 0.5f;
+    private float nextFireTime = 0f;
+    private float speedPlayer = 10f;
+    public Transform GunPoint;
+
+
     void Start()
     {
         
     }
+
+    void Shoot(Vector2 direction)
+    {
+        GameObject bullet = Instantiate(bulletPrefab, GunPoint.position, Quaternion.identity);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.velocity = direction.normalized * bulletSpeed;
+    }
+
+    void OnTrigger(Collider2D other)
+    {
+        if (other.CompareTag("Wall"))
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
 
     void Update()
     {
@@ -22,5 +48,11 @@ public class Player : MonoBehaviour
 
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, angle + offsetAngle);
+
+        if (Input.GetMouseButton(0) && Time.time > nextFireTime)
+        {
+            Shoot(direction);
+            nextFireTime = Time.time + fireRate;
+        }
     }
 }
