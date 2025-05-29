b7cf094d0ac13545e27667c5bfdf847c74532345
Nguyễn Vinh Hiển <shcct@MacBook-Air-cua-Hien.local>
2025-05-29 11:00:49 +0700

    update friction

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -12,7 +12,7 @@ public class Player : MonoBehaviour
     private float nextFireTime = 0f;
     public float recoilForce = 50f;
     public float maxSpeed = 50f;
-    public float friction = 0.98f;
+    public float friction = 0.995f;
     private Rigidbody2D rb;
     public Transform GunPoint;
 
